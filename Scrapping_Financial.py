91cd157724105ff9cab5596cc4c351ae158f4e64
GeraMartinez94 <geramartinez450@gmail.com>
2025-05-20 14:03:25 -0300

    Updated  comments in file

--- Scrapping_Financial.py
+++ Scrapping_Financial.py
@@ -4,16 +4,24 @@ import pandas as pd
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
 
-url = "https://www.geopriskindex.com/results-final-risk-index/"
+# --- Configuración  #
+URL = "https://www.geopriskindex.com/results-final-risk-index/"
+EXCEL_FILENAME = "formato_wide_geopriskinder.xlsx"
+SHEET_NAME = 'Índice de Riesgo Global'
+COUNTRY_COLUMN_NAME = '\ufeffCountry'
 
 try:
-    response = requests.get(url)
+    # Obtiene los datos de la web #
+    response = requests.get(URL)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'html.parser')
     table = soup.find('table')
 
     if table:
+        # Extrae los encabezados #
         headers = [th.text.strip() for th in table.find_all('th')]
+
+        # Extraer datos de filas #
         data = []
         tbody = table.find('tbody')
         if tbody:
@@ -24,46 +32,47 @@ try:
             print("No se encontró el cuerpo de la tabla (tbody).")
             exit()
 
+        # Crea DataFrame inicial #
         df = pd.DataFrame(data, columns=headers)
         print("Encabezados del DataFrame:", headers)
 
-        # Crear el DataFrame en formato ancho
+        # Crear DataFrame en formato ancho #
         unique_years = df['Year'].unique()
-        country_column_name = '\ufeffCountry'
-
-        unique_countries = df[country_column_name].unique()
+        unique_countries = df[COUNTRY_COLUMN_NAME].unique()
         df_wide = pd.DataFrame()
         df_wide['Date'] = [f"{year}-mm-dd" for year in unique_years for _ in unique_countries]
         df_wide['Country'] = [country for _ in unique_years for country in unique_countries]
 
         for header in headers:
-            if header not in [country_column_name, 'Year', 'Region']:
+            if header not in [COUNTRY_COLUMN_NAME, 'Year', 'Region']:
                 values = []
                 for year in unique_years:
                     df_year = df[df['Year'] == year]
-                    country_value_map = df_year.set_index(country_column_name)[header].to_dict()
+                    country_value_map = df_year.set_index(COUNTRY_COLUMN_NAME)[header].to_dict()
                     for country in unique_countries:
                         values.append(country_value_map.get(country, ''))
                 df_wide[header] = values
 
-        excel_filename = "formato_wide_geopriskinder.xlsx"
-        sheet_name = 'Índice de Riesgo Global'
-        df_wide.to_excel(excel_filename, index=False, sheet_name=sheet_name)
-        print(f"Se creo el archivo Excel: {excel_filename}")
-        workbook = load_workbook(excel_filename)
-        sheet = workbook[sheet_name]
+        # Guarda el DataFrame en  archivo Excel #
+        df_wide.to_excel(EXCEL_FILENAME, index=False, sheet_name=SHEET_NAME)
+        print(f"Se creo el archivo Excel: {EXCEL_FILENAME}")
+
+        # Ajusta ancho de columnas en Excel #
+        workbook = load_workbook(EXCEL_FILENAME)
+        sheet = workbook[SHEET_NAME]
+
         for column_cells in sheet.columns:
             max_len = 0
             column = [cell.value for cell in column_cells]
             try:
                 max_len = max(len(str(value)) for value in column if value is not None)
             except ValueError:
-                pass 
+                pass
 
-            adjusted_width = (max_len + 2) 
+            adjusted_width = max_len + 2
             sheet.column_dimensions[get_column_letter(column_cells[0].column)].width = adjusted_width
 
-        workbook.save(excel_filename)
+        workbook.save(EXCEL_FILENAME)
 
     else:
         print("No se encontró ninguna tabla en la página.")
